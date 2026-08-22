7bf4b9724367cb37d18df83ccfbaf93ef6e7038b
elouanzer <127297713+elouanzer@users.noreply.github.com>
2026-08-22 18:44:55 +0200

    fix: brevo mailing

--- src/email.py
+++ src/email.py
@@ -10,7 +10,9 @@ load_dotenv()
 SMTP_SERVER = os.getenv("SMTP_SERVER")
 SMTP_PORT = int(os.getenv("SMTP_PORT"))
 EMAIL_SENDER = os.getenv("EMAIL_SENDER")
-PASSWORD = os.getenv("PASSWORD")
+SMTP_PASSWORD = os.getenv("SMTP_PASSWORD")
+SMTP_LOGIN = os.getenv("SMTP_LOGIN")
+
 URL_LOGO = "https://cdn.jsdelivr.net/gh/elouanzer/paris-cine-alert@main/assets/paris_cine_alert_logo_no_background.png"
 
 class Mailer:
@@ -83,8 +85,8 @@ class Mailer:
         
         try:
             print("Connecting to SMTP...")
-            with smtplib.SMTP_SSL(SMTP_SERVER, SMTP_PORT) as server:
-                server.login(EMAIL_SENDER, PASSWORD)
+            with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
+                server.login(SMTP_LOGIN, SMTP_PASSWORD)
                 server.send_message(msg)
                 print(f"Email sent to {recipient}.")
                 
